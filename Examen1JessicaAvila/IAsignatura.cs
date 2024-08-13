a613113db48bdde0f7b1c10486e5092034a78fc8
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-08-12 20:15:59 -0600

    Creando la interfaz asignatura

---
+++ Examen1JessicaAvila/IAsignatura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1JessicaAvila
+{
+    public interface IAsignatura
+    {
+        //este metodo sumara las 3 notas parciales y retorna la nota final
+        double CalcularNotaFinal();
+
+
+        //este metodo sumara las 3 notas parciales a partir de los parametros
+        double CalcularNotaFinal(double N1, double N2, double N3);
+
+        //retorna el msj de acuerdo a la nota final
+        string MensajeNotaFinal(double notaFinal);
+
+        //Mostrar en pantalla los datos del alumno, la asignatura y la nota final
+        void Imprmir();
+
+    }
+}
