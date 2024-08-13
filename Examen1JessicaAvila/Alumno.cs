e1aa9e3900200756ab4ec643d86c60e48bab5208
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-08-12 20:04:00 -0600

    Creando la clase Alumno

---
+++ Examen1JessicaAvila/Alumno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1JessicaAvila
+{
+    public class Alumno
+    {
+        public string NombreAlumno { get; set; }
+        public string NumeroCuenta { get; set;}
+        public string Email { get; set;}
+
+        public Alumno() { }
+
+        public Alumno (string NombreAlumno, string NumeroCuenta, string Email)
+        {
+            this.NombreAlumno=NombreAlumno;
+            this.NumeroCuenta=NumeroCuenta;
+            this.Email=Email;
+        }
+
+    }
+}
